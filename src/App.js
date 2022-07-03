cb9de6c50ad50c186e9b6c2329999e62711be9c2
Sanjay Varma <sanjay@billionlives.in>
2022-07-03 17:47:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { Button, FormControl, Grid, InputLabel, MenuItem, Select, Stack } from '@mui/material'
+import PriceCard from './priceCard'
+import { Container } from '@mui/system';
+export const APIKEY = '4089ff3842473995346ee2b23624aaf72504146d052d4ac10dc4ebec5b84ffb4'
 
 function App() {
+  const [symbol, setSymbol] = useState('');
+  const [symList, setSymList] = useState({});
+
+  useEffect(() => {
+    fetch(`https://min-api.cryptocompare.com/data/blockchain/list?api_key=${APIKEY}`)
+      .then((r) => r.json())
+      .then((res) => {
+        setSymList(Object.keys(res.Data).reduce((o, i) => { o[i] = false; return o }, {}));
+        console.log(`loaded ${Object.keys(res.Data).length} symbols`)
+      })
+  }, [])
+
+  const handleChange = (event) => {
+    setSymbol(event.target.value)
+  }
+
+  const addSym = () => {
+    symList[symbol] = true;
+    setSymList({ ...symList })
+  }
+
+  const removeSym = (sym) => {
+    symList[sym] = false;
+    setSymList({ ...symList })
+  }
+
+  const showCard = (sym) => {
+    return <div key={sym}>
+      <PriceCard key={sym} sym={sym} removeSym={() => removeSym(sym)} />
+    </div>
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Stack spacing={2} direction="row">
+        <FormControl>
+          <InputLabel>Symbol</InputLabel>
+          <Select
+            value={symbol}
+            label="Symbol"
+            onChange={handleChange}
+          >
+            {Object.keys(symList).map((s) => {
+              return <MenuItem key={s} value={s}>{s}</MenuItem>
+            })}
+          </Select>
+        </FormControl>
+        <Button variant='contained' onClick={addSym}>Add</Button>
+      </Stack>
+      <Container maxWidth={false}>
+        <Grid sx={{ flexGrow: 1 }} spacing={0} container justifyContent="space-evenly">
+          {
+            Object.keys(symList).filter((i) => { return symList[i] }).map((s) => {
+              return showCard(s)
+            })
+          }
+        </Grid>
+      </Container>
+    </div >
   );
 }
 
---
+++ src/priceCard.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react'
+import { Button, Card, CardContent, CardActions, Grid, Typography } from '@mui/material';
+import { APIKEY } from './App';
+
+function PriceCard(props) {
+    const [price, setPrice] = useState({});
+
+    const updatePrice = () => {
+        fetch(`https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=INR,USD,JPY&api_key=${APIKEY}`)
+            .then((res) => res.json())
+            .then((res) => setPrice(res))
+    }
+
+    useEffect(() => {
+        let id = setInterval(updatePrice, 3000);
+        return () => clearInterval(id);
+    })
+
+    return (
+        <Grid item>
+            <Card>
+                <CardContent>
+                    <Typography variant="h3" component="div">
+                        {props.sym}
+                    </Typography>
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                        {Object.keys(price).map((curr) => {
+                            return <span key={curr}><strong>{curr}:&nbsp;</strong>{price[curr]}<br /></span>
+                        })}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button variant='contained' onClick={() => props.removeSym(props.sym)}>Remove</Button>
+                </CardActions>
+            </Card>
+        </Grid>
+    );
+}
+
+export default PriceCard;
